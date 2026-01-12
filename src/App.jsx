c97676dfaa4de54823207a6f2d8fad0b129ac97f
ramgit <ramakrishna@incrosys.com>
2026-01-12 15:41:34 +0530

    web

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,7 @@ import CloudComputingServicesPage from './ItServicesOptions/CloudComputingServic
 import ItStrategyPlanningPage from './ItServicesOptions/ItStrategyPlanning';
 import HrConsultingPage from './HrServicesOptions/HrConsulting';
 import EmployeeTrainingDevelopmentPage from './HrServicesOptions/EmployeeTrainingDevelopment';
-import RecruitmentTalentAcquisitionPage from './HrServicesOptions/RecruitmentTalentAcquisition';
-import JobApplicationForm from './HrServicesOptions/JobApplicationForm';
+
 
 function App(){
 return(
@@ -32,8 +31,7 @@ return(
         <Route path="/it-strategy-planning" element={<ItStrategyPlanningPage />} />
         <Route path="/hr-consulting" element={<HrConsultingPage />} />
         <Route path="/employee-training-development" element={<EmployeeTrainingDevelopmentPage />} />
-        <Route path="/recruitment-talent-acquisition" element={<RecruitmentTalentAcquisitionPage />} />
-        <Route path="/job-applicatio" element={<JobApplicationForm />} />
+        
       </Routes>
     </>
   );
